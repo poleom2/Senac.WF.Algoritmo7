ef57618e9e5f95a98544b52713ac227b260e6aeb
Miguel <poleom2@gmail.cpm>
2025-04-15 21:33:30 -0300

    completar  el panel de la pantallos de un usuario

--- WF.Login/WF.Login/Form1.Designer.cs
+++ WF.Login/WF.Login/Form1.Designer.cs
@@ -33,11 +33,13 @@
             pictureBox1 = new PictureBox();
             pictureBox2 = new PictureBox();
             pictureBox3 = new PictureBox();
-            textBox1 = new TextBox();
             textBox2 = new TextBox();
             button1 = new Button();
             linkLabel1 = new LinkLabel();
             linkLabel2 = new LinkLabel();
+            textBox3 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
             panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
@@ -58,71 +60,63 @@
             // 
             pictureBox1.BackColor = Color.AliceBlue;
             pictureBox1.Image = (Image)resources.GetObject("pictureBox1.Image");
-            pictureBox1.Location = new Point(3, 3);
+            pictureBox1.Location = new Point(3, 6);
             pictureBox1.Name = "pictureBox1";
             pictureBox1.Size = new Size(320, 444);
             pictureBox1.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox1.TabIndex = 0;
             pictureBox1.TabStop = false;
+            pictureBox1.Click += pictureBox1_Click;
             // 
             // pictureBox2
             // 
             pictureBox2.Image = Properties.Resources.amarillo;
-            pictureBox2.Location = new Point(731, 0);
+            pictureBox2.Location = new Point(855, 0);
             pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(32, 31);
+            pictureBox2.Size = new Size(34, 31);
             pictureBox2.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox2.TabIndex = 1;
             pictureBox2.TabStop = false;
+            pictureBox2.Click += pictureBox2_Click;
             // 
             // pictureBox3
             // 
-            pictureBox3.Image = Properties.Resources.rojo;
-            pictureBox3.Location = new Point(759, 0);
+            pictureBox3.Image = (Image)resources.GetObject("pictureBox3.Image");
+            pictureBox3.Location = new Point(886, 0);
             pictureBox3.Name = "pictureBox3";
-            pictureBox3.Size = new Size(40, 31);
+            pictureBox3.Size = new Size(32, 31);
             pictureBox3.SizeMode = PictureBoxSizeMode.Zoom;
             pictureBox3.TabIndex = 2;
             pictureBox3.TabStop = false;
-            // 
-            // textBox1
-            // 
-            textBox1.BackColor = SystemColors.ActiveCaptionText;
-            textBox1.BorderStyle = BorderStyle.None;
-            textBox1.Font = new Font("Arial", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox1.ForeColor = SystemColors.GrayText;
-            textBox1.Location = new Point(446, 180);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(252, 37);
-            textBox1.TabIndex = 3;
-            textBox1.TabStop = false;
-            textBox1.Text = "USUARIO";
-            textBox1.TextAlign = HorizontalAlignment.Center;
+            pictureBox3.Click += pictureBox3_Click;
             // 
             // textBox2
             // 
-            textBox2.BackColor = SystemColors.InactiveCaptionText;
+            textBox2.BackColor = SystemColors.ButtonShadow;
             textBox2.BorderStyle = BorderStyle.None;
             textBox2.Font = new Font("Arial", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            textBox2.ForeColor = SystemColors.GrayText;
-            textBox2.Location = new Point(452, 242);
+            textBox2.ForeColor = SystemColors.Control;
+            textBox2.Location = new Point(405, 209);
             textBox2.Name = "textBox2";
-            textBox2.Size = new Size(246, 37);
-            textBox2.TabIndex = 4;
+            textBox2.Size = new Size(394, 37);
+            textBox2.TabIndex = 2;
             textBox2.Text = "SENHA";
-            textBox2.TextAlign = HorizontalAlignment.Center;
+            textBox2.Enter += textBox2_Enter;
+            textBox2.Leave += textBox2_Leave;
             // 
             // button1
             // 
-            button1.BackColor = SystemColors.ActiveCaptionText;
-            button1.FlatStyle = FlatStyle.Popup;
+            button1.BackColor = SystemColors.ButtonShadow;
+            button1.FlatAppearance.BorderColor = Color.Gray;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.FlatStyle = FlatStyle.Flat;
             button1.Font = new Font("Arial", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            button1.ForeColor = SystemColors.GrayText;
-            button1.Location = new Point(452, 311);
+            button1.ForeColor = SystemColors.ControlLight;
+            button1.Location = new Point(501, 281);
             button1.Name = "button1";
             button1.Size = new Size(246, 48);
-            button1.TabIndex = 5;
-            button1.Text = "LOGIN";
+            button1.TabIndex = 3;
+            button1.Text = "ENTRAR";
             button1.UseVisualStyleBackColor = false;
             button1.Click += button1_Click;
             // 
@@ -139,31 +133,69 @@
             // 
             linkLabel2.AutoSize = true;
             linkLabel2.Font = new Font("Arial Narrow", 14F, FontStyle.Regular, GraphicsUnit.Point, 0);
-            linkLabel2.Location = new Point(477, 378);
+            linkLabel2.Location = new Point(483, 378);
             linkLabel2.Name = "linkLabel2";
-            linkLabel2.Size = new Size(203, 33);
-            linkLabel2.TabIndex = 7;
+            linkLabel2.Size = new Size(264, 33);
+            linkLabel2.TabIndex = 0;
             linkLabel2.TabStop = true;
-            linkLabel2.Text = "Esqueceo a senha";
+            linkLabel2.Text = "Esquece da ninha senha";
             linkLabel2.LinkClicked += linkLabel2_LinkClicked;
+            // 
+            // textBox3
+            // 
+            textBox3.BackColor = SystemColors.ButtonShadow;
+            textBox3.BorderStyle = BorderStyle.None;
+            textBox3.Font = new Font("Arial", 16F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            textBox3.ForeColor = SystemColors.Control;
+            textBox3.Location = new Point(405, 138);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(406, 37);
+            textBox3.TabIndex = 1;
+            textBox3.Text = "USUARIO";
+            textBox3.Enter += textBox3_Enter;
+            textBox3.Leave += textBox3_Leave;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.ForeColor = SystemColors.ButtonFace;
+            label1.Location = new Point(395, 159);
+            label1.Name = "label1";
+            label1.Size = new Size(416, 25);
+            label1.TabIndex = 9;
+            label1.Text = " _________________________________________________________";
+            label1.Click += label1_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.ForeColor = SystemColors.ButtonFace;
+            label2.Location = new Point(405, 253);
+            label2.Name = "label2";
+            label2.Size = new Size(416, 25);
+            label2.TabIndex = 10;
+            label2.Text = " _________________________________________________________";
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            BackColor = SystemColors.ActiveCaptionText;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ButtonShadow;
+            ClientSize = new Size(917, 450);
+            Controls.Add(textBox2);
+            Controls.Add(label2);
+            Controls.Add(textBox3);
+            Controls.Add(label1);
             Controls.Add(linkLabel2);
             Controls.Add(linkLabel1);
             Controls.Add(button1);
-            Controls.Add(textBox2);
-            Controls.Add(textBox1);
             Controls.Add(pictureBox3);
             Controls.Add(pictureBox2);
             Controls.Add(panel1);
+            ForeColor = Color.Black;
             FormBorderStyle = FormBorderStyle.None;
             Name = "Form1";
-            Opacity = 0.85D;
+            Opacity = 0.9D;
             Text = "Form1";
             panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
@@ -179,10 +211,12 @@
         private PictureBox pictureBox1;
         private PictureBox pictureBox2;
         private PictureBox pictureBox3;
-        private TextBox textBox1;
         private TextBox textBox2;
         private Button button1;
         private LinkLabel linkLabel1;
         private LinkLabel linkLabel2;
+        private TextBox textBox3;
+        private Label label1;
+        private Label label2;
     }
 }
--- WF.Login/WF.Login/Form1.cs
+++ WF.Login/WF.Login/Form1.cs
@@ -2,6 +2,8 @@ namespace WF.Login
 {
     public partial class Form1 : Form
     {
+        private object messageBoxButtons;
+
         public Form1()
         {
             InitializeComponent();
@@ -19,6 +21,56 @@ namespace WF.Login
 
         private void button1_Click(object sender, EventArgs e)
         {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_Enter(object sender, EventArgs e)
+        {
+            if (textBox3.Text == "USUARIO")
+                textBox3.Text = String.Empty;
+        }
+
+        private void textBox3_Leave(object sender, EventArgs e)
+        {
+            if (textBox3.Text == string.Empty)
+                textBox3.Text = "USUARIO";
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+            if (textBox2.Text == "SENHA")
+                textBox2.Text = string.Empty;
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+            if (textBox2.Text == string.Empty)
+                textBox2.Text = "SENHA";
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {   
+            var resposta=MessageBox.Show("Deseja encerrar o programa",
+                "Sair",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+            if(resposta == DialogResult.Yes)
+            Application.Exit();
 
         }
     }
